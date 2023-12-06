c7c6acf1495e4373d7d405b78e373d3be2acfb9f
ImOmkar <omkar211196@gmail.com>
2023-12-06 16:35:03 +0530

    Browsable API kadhun takla

--- FactsAPI/settings.py
+++ FactsAPI/settings.py
@@ -58,6 +58,12 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'FactsAPI.urls'
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+}
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
